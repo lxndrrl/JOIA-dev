d724a1e9e5825704925865fbe82105887b8be8f0
Alexander Orel <lxndrrl@gmail.com>
2014-05-27 21:30:04 +0300

    Удалил папку "assets"
    Если будет необходимо, всегда могу вернуть.

--- www/js/common.js
+++ www/js/common.js
@@ -24,4 +24,26 @@ head.ready(function() {
 	});
 
     $('.search').search();
+
+	$(document).ready(function() {
+		$("[data-toggle]").click(function() {
+			var toggle_el = $(this).data("toggle");
+			$(toggle_el).toggleClass("open-sidebar");
+		});
+
+	});
+
+	$(".swipe-area").swipe({
+		swipeStatus:function(event, phase, direction, distance, duration, fingers)
+		{
+			if (phase=="move" && direction =="right") {
+				$(".top").addClass("open-nav-mob");
+				return false;
+			}
+			if (phase=="move" && direction =="left") {
+				$(".top").removeClass("open-nav-mob");
+				return false;
+			}
+		}
+	});
 });
