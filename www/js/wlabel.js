7e8908ce93c289b6b32d525e5f0c294c34eda403
Alexander Orel <lxndrrl@gmail.com>
2014-06-06 21:30:27 +0300

    в HTML meta viewport и правки в o_proekte.html

--- www/js/wlabel.js
+++ www/js/wlabel.js
@@ -6,3 +6,19 @@ $('.action_collapse').click(function(e){
 	$('.choosed_list_content').slideUp();
 	e.preventDefault();
 })
+
+$('.about_project_content .toggle_link').click(function(e){
+	$('.about_project_content .hided_content').slideToggle();
+	$(this).toggleClass('arrow_rotate').children('span').toggle();
+	e.preventDefault();
+})
+$('.appeal_content .toggle_link').click(function(e){
+	$(this).parent('.appeal_content').hide();
+	$('.appeal_hiden_content').slideDown();
+	e.preventDefault();
+})
+$('.appeal_hiden_content .toggle_link').click(function(e){
+	$(this).parent('.appeal_hiden_content').slideUp();
+	$('.appeal_content').show();
+	e.preventDefault();
+})
