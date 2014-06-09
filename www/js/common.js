fdd11c5e43f449700a00f2e0e61941d4807e13c1
Alexander Orel <lxndrrl@gmail.com>
2014-06-09 21:26:02 +0300

    Временные изменения

--- www/js/common.js
+++ www/js/common.js
@@ -31,11 +31,16 @@ head.ready(function () {
 	/*/расширяемый блок*/
 
 	/*блок на всю высоту вьюпорта*/
-	$(window).resize(function () {
+	/*$(window).resize(function () {
 		$('.w-900 .superslider__slide, .lt-900 .superslider__slide').height($(window).height());
 	});
-	$('.w-900 .superslider__slide, .lt-900 .superslider__slide').height($(window).height());
+	$('.w-900 .superslider__slide, .lt-900 .superslider__slide').height($(window).height());*/
 	/*/блок на всю высоту вьюпорта*/
+
+	$(window).resize(function(){
+		$(".top").height($(window).height());
+	});
+	$(".top").height($(window).height());
 
 });
 
