12dd8e64083fb1544f53578abbcca8d354b6c44a
Alexander Orel <lxndrrl@gmail.com>
2014-07-27 13:31:06 +0300

    Правильный вид ПопАпа и формы входа

--- assets/popup/popup.js
+++ assets/popup/popup.js
@@ -1,41 +1,42 @@
-  $('#yes').on('click', function() {
-    $.cookie('CLickYesClass', 'click_yes');
-  });
+$('#yes').on('click', function () {
+	$.cookie('CLickYesClass', 'click_yes');
+});
 
-  $(document).ready(function() {
+$(document).ready(function () {
 
-    // Нажатие на кнопку нет
-    $('.popup__content__no #no').on('click', function() {
-      window.location = $(this).val();
-    });
+	// Нажатие на кнопку нет
+	$('.popup__content__no #no').on('click', function () {
+		window.location = $(this).val();
+	});
 
-    // Для начала вытащим popup
-    $('body > .blur').addClass('blur_-disallow');
-    $('#popup').css('display', 'block');
+	// Для начала вытащим popup
+	$('body > .blur').addClass('blur_-disallow');
+	$('#popup').css('display', 'block');
 
-    // Не показывать попап если пользователь нажал на кнопку ДА
-    if ($.cookie('CLickYesClass') === 'click_yes') {
-      $('#popup').css('display', 'none');
-      $('#popup').siblings('body > .blur').addClass('blur_-allow');
-      $('#popup').siblings('body > .blur').removeClass('blur_-disallow');
-    }
-    else {
+	// Не показывать попап если пользователь нажал на кнопку ДА
+	if ($.cookie('CLickYesClass') === 'click_yes') {
+		$('#popup').css('display', 'none');
+		$('#popup__content').css('display', 'none');
+		$('#popup').siblings('body > .blur').addClass('blur_-allow');
+		$('#popup').siblings('body > .blur').removeClass('blur_-disallow');
+	}
+	else {
 
-      /*Оверлей ПопАпа и события на кнопки*/
-      $('.popup__content').click(function (e) {
-        e.stopPropagation();
-      });
-      var sbula = $(".popup label");
-      sbula.click(function () {
-        sbula.removeClass("active").not(sbula).add(this).addClass("active");
-      });
-      $('.popup__content__yes label').click(function () {
-        $('.popup').fadeOut(1000);
-        $('.blur').addClass('blur_-allow');
-        $('.popup__content').fadeOut(1000);
-      });
+		/*Оверлей ПопАпа и события на кнопки*/
+		$('.popup__content').click(function (e) {
+			e.stopPropagation();
+		});
+		var sbula = $(".popup label");
+		sbula.click(function () {
+			sbula.removeClass("active").not(sbula).add(this).addClass("active");
+		});
+		$('.popup__content__yes label').click(function () {
+			$('.popup').fadeOut(1000);
+			$('.blur').addClass('blur_-allow');
+			$('.popup__content').fadeOut(1000);
+		});
 
 
-      /*/Оверлей ПопАпа и события на кнопки*/
-    }
-  });
+		/*/Оверлей ПопАпа и события на кнопки*/
+	}
+});
--- assets/signin-form/signin-form.js
+++ assets/signin-form/signin-form.js
@@ -2,6 +2,7 @@
 $('.login').click(function(){
 	$('.signin-form').removeClass('hidden').addClass('as-table');
 	$('.blur').addClass('blur_-popup-launched');
+//	$('.popup__content').addClass('hidden');
 	$('.popup').fadeIn(1000);
 });
 /// Открыть форму входа
@@ -10,6 +11,7 @@ $('.login').click(function(){
 $('.signin-form__btn-close').click(function(){
 	$('.signin-form').addClass('hidden').removeClass('as-table');
 	$('.blur').removeClass('blur_-popup-launched');
+//	$('.popup__content').removeClass('hidden');
 	$('.popup').fadeOut(1000);
 });
 /// Закрыть форму входа
