aa9c6630adf3716ea2326665ffaf942a429881a9
omarr-G <omarr@list.ru>
2014-08-12 00:54:59 +0400

    При срабатывании размытия полупрозрачный белый фон под формой входа должен остаться

--- assets/signin-form/signin-form.js
+++ assets/signin-form/signin-form.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // Открыть форму входа
   $('.login').click(function(){
     $('.signin-form').removeClass('hidden').removeClass('is-hidden').addClass('as-table');
+    $('html').css('overflow', 'hidden');
     // $('.blur').addClass('blur_-popup-launched');
   //  $('.popup__content').addClass('hidden');
     // $('.popup').fadeIn(1000);
@@ -11,6 +12,7 @@ $(document).ready(function() {
   // Закрыть форму входа
 
   $('.signin-form__btn-close').on('click', function() {
+    $('html').css('overflow', 'auto');
     $('.signin-form').addClass('is-hidden');
     setTimeout(function () {
       $('.signin-form').addClass('hidden');
